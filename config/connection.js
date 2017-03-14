77859afe820203d81cc7e0ce6c0bfa4055d8988d
Justin Bellero <jpbellero@gmail.com>
2017-03-14 12:54:57 -0400

    init application skeleton

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+// DEPENDANCIES---------------------
+const mysql = require('mysql');
+
+//----------------------------------
+console.log("|-------------------------------\n");
+console.log("Registering a connection to the DB");
+console.log("|");
+console.log("|");
+console.log("|-------------------------------\n");
+
+const options = {
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || 3306,
+    username: process.env.MYSQL_USER || 'developer',
+    password: process.env.MYSQL_PASSWORD || null,
+    database: 'burger_db'
+}
+
+const db = mysql.createConnection(options);
+
+// On Connect --------------------------------
+db.connect((err) => {
+    if err throw new Error(err);
+
+    console.log(`Connected to DB @${options.host}:${options.port}`);
+});
+
+module.exports = db;
