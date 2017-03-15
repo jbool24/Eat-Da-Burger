aec1ab5d7501b949f5a800386274460e0420ea23
Justin Bellero <jpbellero@gmail.com>
2017-03-15 11:03:10 -0400

    fixes burgerapp refresh function

--- app/public/js/burgerApp.js
+++ app/public/js/burgerApp.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    console.log("app client loaded");
-
     var baseUrl = window.location.origin;
     var url = `${baseUrl}/api/burgers/devoured`;
 
+    // Get Burgers on load
     $.get(url, null, (burgers) => {
         var ul = $(".burgers-eaten");
         burgers.forEach((burger) => {
@@ -12,32 +11,35 @@ $(document).ready(function() {
         });
     });
 
+    // Eat Button Click Listener
     $(".order-window").on('click', "button", () => {
         var burgerID = $(this.activeElement).attr('data-id');
-        console.log(burgerID);
         $.post(`${baseUrl}/api/burger/${burgerID}?_method=PUT`).done((response) => {
-          console.log(response);
-          refreshOrders()
-        })
+            refreshOrders();
+        });
     });
 
-    function submitOrder(event) {
-        event.preventDefault();
-        console.log('clicked order');
-    }
-
-    function refreshOrders(){
-      $.get(`${baseUrl}/api/burgers`).done((response)=>{
-        var orders = $(".burger-list")
-        orders.empty();
-        response.forEach((burger)=>{
-          var list = $("<ul>");
-          var eatBtn =$("<button>");
-          eatBtn.attr('data-id', burger.id);
-
-          list.append(`<li>${burger.burger_name}${eatBtn}<li>`)
+    function refreshOrders() {
+        // Refresh burger list
+        $.get(`${baseUrl}/api/burgers`).done((burgers) => {
+            var orders = $(".burger-list");
+            orders.empty();
+            burgers.forEach((burger) => {
+                var list = $("<ul>");
+                var eatBtn = $("<button>");
+                eatBtn.attr('data-id', burger.id);
+                list.append(`<li>${burger.burger_name}${eatBtn}<li>`)
+            });
         });
 
-      })
+        // Refresh burgers eaten
+        $.get(`${baseUrl}/api/burgers/devoured`).done((burgerList) => {
+            var list = $(".burgers-eaten");
+            list.empty();
+            burgerList.forEach((burger) => {
+                list.append(`<li><i class='fa fa-trash'> ${burger.burger_name}</i><li>`);
+            });
+        });
     }
+
 });
